2ab945c7801065681c5480d363eb23eba450689e
James Cuzella <james.cuzella@lyraphase.com>
2026-04-02 00:26:35 -0600

    templates/**/secrets.nix: Switch agenix -> sops-nix

--- templates/starter-with-secrets/modules/darwin/secrets.nix
+++ templates/starter-with-secrets/modules/darwin/secrets.nix
@@ -1,37 +1,31 @@
-{ config, pkgs, agenix, secrets, ... }:
+{ config, pkgs, sops-nix, secrets, ... }:
 
 let user = "%USER%"; in
 {
-  age.identityPaths = [
-    "/Users/${user}/.ssh/id_ed25519"
-  ];
+  sops = {
+    defaultSopsFile = "${secrets}/secrets.yaml";
+    defaultSopsFormat = "yaml";
 
-  # Your secrets go here
-  #
-  # Note: the installWithSecrets command you ran to boostrap the machine actually copies over
-  #       a Github key pair. However, if you want to store the keypair in your nix-secrets repo
-  #       instead, you can reference the age files and specify the symlink path here. Then add your
-  #       public key in shared/files.nix.
-  #
-  #       If you change the key name, you'll need to update the SSH configuration in shared/home-manager.nix
-  #       so Github reads it correctly.
+    gnupg.home = "/Users/${user}/.gnupg";
+    gnupg.sshKeyPaths = [];
 
-  #
-  # age.secrets."github-ssh-key" = {
-  #   symlink = true;
-  #   path = "/Users/${user}/.ssh/id_github";
-  #   file =  "${secrets}/github-ssh-key.age";
-  #   mode = "600";
-  #   owner = "${user}";
-  #   group = "staff";
-  # };
+    # Your secrets go here
+    #
+    # Example:
 
-  # age.secrets."github-signing-key" = {
-  #   symlink = false;
-  #   path = "/Users/${user}/.ssh/pgp_github.key";
-  #   file =  "${secrets}/github-signing-key.age";
-  #   mode = "600";
-  #   owner = "${user}";
-  # };
-
+    # secrets = {
+    #   "github-ssh-key" = {
+    #     path = "/Users/${user}/.ssh/id_github";
+    #     mode = "0600";
+    #     owner = "${user}";
+    #     group = "staff";
+    #   };
+    #
+    #   "github-signing-key" = {
+    #     path = "/Users/${user}/.ssh/pgp_github.key";
+    #     mode = "0600";
+    #     owner = "${user}";
+    #   };
+    # };
+  };
 }
